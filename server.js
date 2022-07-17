0298442207eafd940915018016a8d2a7af504cfa
MikeDePacina <mikedpacina@gmail.com>
2022-07-16 17:23:28 -0700

    finished author routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/author");
 const bookRouter = require("./routes/books");
+const methodOverride = require("method-override");
 
 app.use(express.json());
 app.use(express.urlencoded({ limit: "10mb", extended: false }));
@@ -15,6 +16,7 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 const mongoose = require("mongoose");
 
